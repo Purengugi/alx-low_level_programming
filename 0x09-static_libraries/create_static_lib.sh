51f9c35efc9bb1854692e30417df1c172c75045b
Purengugi <Purity2004ngugi@gmail.com>
2023-05-12 10:06:40 -0700

    update

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -c *.c ar -rc liball.a *.o
-ranlib liball.a
+
+# Compile each .c file into corresponding .o files
+gcc -c *.c
+
+# Create the static library
+ar rcs liball.a *.o
+
+# List the contents of the static library
+ar -t liball.a
+
+# Remove the .o files
+rm *.o
