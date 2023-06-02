5e0e9506f53d6d526cd6cf2830edfc5b7866af80
Purengugi <purity2004ngugi@gmail.com>
2023-06-02 04:02:51 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,25 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
-* print_list - a function that prints all elements of a singly-linked list
-* @h: a pointer to a list_t list
-* Return: returns the number of nodes
-*/
+ * print_list - check the code for Holberton School students.
+ * @h: name of the list
+ * Return: the number of nodes.
+ */
 size_t print_list(const list_t *h)
 {
-	size_t i = 0;
+	int count = 0;
 
-	if (h == NULL)
-		return (0);
-
-	 while (h != NULL)
+	while (h)
 	{
-		if (h->str != NULL)
-			printf("[%u] %s\n", h->len, h->str);
-		else
+		if (h->str == NULL)
+		{
 			printf("[0] (nil)\n");
+		}
+		else
+		{
+		printf("[%d] %s\n", h->len, h->str);
+		}
+		count++;
 		h = h->next;
-		i++;
 	}
-	return (i);
+	return (count);
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,17 +1,16 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
-* struct list_s - singly linked list
-* @str: string - (malloc'ed string)
-* @len: length of the string
-* @next: points to the next node
-*
-* Description: singly linked list node structure
-*/
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
 typedef struct list_s
 {
 	char *str;
@@ -25,4 +24,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif /* LISTS_H */
+#endif
