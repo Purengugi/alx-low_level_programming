2f6ccad12d01737afd98391ed015725444e4a4df
Purengugi <purity2004ngugi@gmail.com>
2023-06-05 12:44:29 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,38 +1,40 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
-* delete_nodeint_at_index - a function that deletes the node at
-* index of a listint_t linked list
-* @head: a pointer to the head of the list
-* @index: the index of the node to be deleted
-* Return: returns 1 if it succeeded, -1 if it failed
-*/
+ * delete_nodeint_at_index - deletes the node at given index
+ *                           of a listint_t linked list.
+ * @head: double pointer to the first head node in the list
+ * @index: index of the node to delete
+ *
+ * Return: (1) if success ELSE (-1) if failure
+ */
+
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	unsigned int i = 0;
-	listint_t *tmp = *head;
-	listint_t *current = NULL;
+	listint_t *temp, *copy = *head;
+	unsigned int node;
 
-	 if (!(*head))
+	if (copy == NULL)
 		return (-1);
 
-	 if (index == 0)
+	if (index == 0)
 	{
-		tmp = (*head)->next;
-		free(*head);
-		*head = tmp;
+		*head = (*head)->next;
+		free(copy);
 		return (1);
 	}
 
-	for (i = 0; i < index - 1; i++)
+	for (node = 0; node < (index - 1); node++)
 	{
-		if (!tmp || !(tmp->next))
+		if (copy->next == NULL)
 			return (-1);
-		tmp = tmp->next;
-	}
-	current = tmp->next;
-	tmp->next = current->next;
-	free(current);
 
+		copy = copy->next;
+	}
+
+	temp = copy->next;
+	copy->next = temp->next;
+	free(temp);
 	return (1);
 }
