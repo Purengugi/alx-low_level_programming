a380cc06012c453e4f68d0ad6478647df93f9bfd
Purengugi <purity2004ngugi@gmail.com>
2023-07-20 11:07:41 -0700

    update

--- 0x17-doubly_linked_lists/102-result.py
+++
@@ -1,6 +0,0 @@
-# Number to save in binary format (906609 in decimal)
-number = 906609
-
-# Save the number in binary format as a binary file
-with open("102-result", "wb") as file:
-    file.write(number.to_bytes(6, byteorder='big'))
