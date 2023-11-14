1cfe6bbe6e224e699abe88abac5dccb095e95af8
Purengugi <purity2004ngugi@gmail.com>
2023-11-14 06:03:41 -0800

    update

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -10,5 +10,6 @@ void print_array(int *array, size_t l, size_t r);
 int binary_search_index(int *array, size_t l, size_t r, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
 
 #endif /* _search_algos_ */
