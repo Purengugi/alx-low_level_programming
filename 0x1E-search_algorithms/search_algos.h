5163eced12a72898702d459d64ced023458ec6c7
Purengugi <purity2004ngugi@gmail.com>
2023-11-14 08:02:59 -0800

    updae

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,6 +3,24 @@
 
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
+
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
+listint_t *jump_list(listint_t *list, size_t size, int value);
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
@@ -11,6 +29,5 @@ int binary_search_index(int *array, size_t l, size_t r, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
-listint_t *jump_list(listint_t *list, size_t size, int value);
 
 #endif /* _search_algos_ */
